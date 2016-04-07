fe5545119f3e5afd0bcc656625975ca017379ad1
Wittkopp <j_witt10@uni-muenster.de>
2016-04-07 10:06:03 +0200

    Äußerliche Veränderungen
    Neue Grundkarte, Informationen umgeschrieben, Standardwerte hinzugefügt

--- js/functions.js
+++ js/functions.js
@@ -13,12 +13,10 @@ var map = L.map('map', {
 		zoomControl : true
 }).setView([51.40, 7.40], 9);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom : 18,
-	attribution : 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-	'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-	'Imagery <a href="http://mapbox.com">Mapbox</a>',
-	id : 'mapbox.streets'
+	attribution : '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	//id : 'mapbox.light'
 }).addTo(map);
 
 var speedTracks = L.tileLayer.wms(WMS + "/arcgis/services/enviroCar/aggregation/MapServer/WMSServer", {
