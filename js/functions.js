b449e7c996a03d53d4b0b2a2e1931b9bacae7f6f
Wittkopp <j_witt10@uni-muenster.de>
2016-06-27 14:54:18 +0200

    GUI angepasst
    Kompabilität IE

--- js/functions.js
+++ js/functions.js
@@ -335,3 +335,35 @@ function buildJsonText(){
     return(text);
     }
 }
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the slidedown Information for Envirocar content */
+function slideDownInfo() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+//Update Buffer in Realtime
+$(function () {
+  var $input = $('#buffer');
+  $input.on('keydown', function () {
+    setTimeout(function () {
+    bufVal = document.getElementById('buffer').value;
+    filterCircle.setRadius($input.val());
+    }, 0);
+  });
+})
